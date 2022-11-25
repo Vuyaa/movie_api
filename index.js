e9966272bd036973e8ffb403560a8d98067a30d4
Vuyaa <st_im@hotmail.com>
2022-11-25 22:50:23 +0100

    allowedOrigins *

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //JSON object holding data about top10movies such as title and author
 
-let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://popkorny.herokuapp.com'];
+let allowedOrigins = ['*'];
 app.use(
   cors({
     origin: (origin, callback) => {
