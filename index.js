78afeb03db92a4ca6ef19f244cdfc495632ca2d1
Bruno Vujevic <113612454+Vuyaa@users.noreply.github.com>
2023-02-16 17:17:22 +0100

    Updated cors

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //JSON object holding data about top10movies such as title and author
 
-let allowedOrigins = ["*"];
+let allowedOrigins = ["*", "https://site--popkorny--w5mfxztkv9bc.code.run/movies" ,];
 app.use(
   cors({
     origin: (origin, callback) => {
