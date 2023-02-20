1b3069641389f9fa53b5f6c4bbd98d315aa01d9b
Bruno Vujevic <113612454+Vuyaa@users.noreply.github.com>
2023-02-20 21:59:22 +0100

    Updated API call

--- index.js
+++ index.js
@@ -106,7 +106,6 @@ app.post(
     if (!errors.isEmpty()) {
       return res.status(422).json({ errors: errors.array() });
     }
-    let hashedPassword = Users.hashPassword(req.body.Password);
     Users.findOne({ Username: req.body.Username })
       .then((user) => {
         if (user) {
@@ -114,7 +113,7 @@ app.post(
         } else {
           Users.create({
             Username: req.body.Username,
-            Password: hashedPassword,
+            Password: req.body.Password,
             Email: req.body.Email,
             Birthday: req.body.Birthday,
           })
